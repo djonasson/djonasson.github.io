883384711d31449e4869509f0d9d768994c0c5aa
Daniel Jonasson <1116766+djonasson@users.noreply.github.com>
2023-11-18 18:03:04 +0100

    Update edit configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,13 +44,13 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: `${repoURL}/tree/main/`,
+          editUrl: `${repoURL}/edit/main/`,
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: `${repoURL}/tree/main/`,
+          editUrl: `${repoURL}/edit/main/`,
         },
         theme: {
           customCss: './src/css/custom.css',
